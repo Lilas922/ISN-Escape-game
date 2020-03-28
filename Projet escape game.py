4b0eac9cc7723fb087f788e271cbc00bf6c09193
Vallep2509 <62103989+Vallep2509@users.noreply.github.com>
2020-03-28 17:46:01 +0100

    Update Projet escape game.py

--- Projet escape game.py
+++ Projet escape game.py
@@ -1,17 +1,31 @@
 import pygame
-from pygame.locals import *
-pygame.init()
-largeur_grille = 10     ## nombre de case
-hauteur_grille = 10
-position_grille_x = 0   ## position
-position_grille_y = 0
+from pygame.locals import *   #importer les constantes de Pygame
+pygame.init()        #initialise tous les modules
 
-fenetre = pygame.display.set_mode(( 10 * 80 , 10 * 80 )) ## 405 largeur/hauteur_grille * largeur/hauteur image
+#largeur_grille = 10  ## nombre de case
+#hauteur_grille = 10
+#position_grille_x = 0  ## position
+#position_grille_y = 0
 
-cellule = pygame.image.load("cellule_de_prison.JPG").convert()
+fenetre = pygame.display.set_mode(( 800 , 800 )) ## affichage fenêtre pygame 405 largeur/hauteur_grille * largeur/hauteur image
+
+cellule = pygame.image.load("cellule800.jpg").convert()  #.convert = conversion de l'image au format de la fenêtre
 fenetre.blit(cellule, (0,0))
 
-lettre = pygame.image.load("lettre.PNG").convert()     ## image lettre
-fenetre.blit(lettre, (130,100))
+lettre = pygame.image.load("lettre.jpg").convert()    ## image lettre
+fenetre.blit(lettre, (104,56))
 
-pygame.display.flip()
+
+pygame.display.flip() #afficher plusieurs images à la fois, "rafraîchir" l'écran pour voir les changements effectués depuis le dernier rafraîchissement
+
+continuer = 1
+
+#Boucle infinie
+while continuer:
+    for event in pygame.event.get():   #On parcours la liste de tous les événements reçus
+        if event.type == QUIT:     #Si un de ces événements est de type QUIT
+            continuer = 0      #On arrête la boucle
+        if event.type == KEYDOWN:
+            if event.key == K_SPACE:
+                
+                
